ed7f906b3689ce55b289fa300e27e4edbcda9e4a
David <david19kerrigan@gmail.com>
2024-05-06 17:47:07 -0400

    auto commit

--- user/find.c
+++ user/find.c
@@ -1,20 +1,38 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
+#include "kernel/fs.h"
 #include "user/user.h"
 
-int
-main(int argc, char *argv[])
-{
-  struct stat st;
-  struct dirent de;
-  char buf[512];
+int find(char *path, char *name){
+  if(fork() == 0){
+    struct dirent de;
+    struct stat st;
 
-  int fd = open(".", 0);
-  fstat(fd, &st);
-  stat(buf, &st);
-  while(read(fd, &de, 1)){
+    int fd = open(path, 0);
+    fstat(fd, &st);
+    if(st.type == T_DIR){
+      while(read(fd, &de, sizeof(de))){
+        if(de.inum == 0) continue;
+        printf("%s %d\n", de.name, st.type);
+        //char buf[512], *new_path;
+        //new_path = buf + strlen(buf);
+        //*new_path++ = '/';
+        //stat(buf, &st);
+        //memcpy(new_path, de.name, DIRSIZ);
+        //new_path[DIRSIZ] = 0;
+        close(fd);
+        return find(path, name);
+      }
+    }
+
+    close(fd);
+    return 0;
+  } else {
+    wait(0);
+    return 0;
   }
+}
 
-  close(fd);
-  exit(0);
+int main(int argc, char *argv[]){
+  find(".", "test");
 }
